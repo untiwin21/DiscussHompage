58cee69e9fef9c76087d8f2b0e5d7d5f238ce2c3
Choi TaeWoong <108115637+untiwin21@users.noreply.github.com>
2023-11-27 12:03:59 +0000

    pratice1

---
+++ src/Login.jsx
@@ -0,0 +1,10 @@
+function Login(){
+    return(
+        <div>
+            <div class="login">아이디를 입력하세요</div>
+            <div class="login">비밀번호를 입력하세요</div>
+        </div>
+    );
+}
+
+export default Login;
